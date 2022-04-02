d0b574595eca0a8091391d6a385b224817a94a5c
kalaibharath <kalaicbi28@gmail.com>
2022-04-02 18:11:30 +0530

    second comit

--- docs/src/component/Navbar.jsx
+++ docs/src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import logo from "../../images/logo.png";
+
 
 const NavBarItem = ({ title, classprops }) => (
     <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
@@ -13,9 +13,7 @@ const Navbar = () => {
 
     return (
         <nav className="w-full flex md:justify-center justify-between items-center p-4">
-            <div className="md:flex-[0.5] flex-initial justify-center items-center">
-                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
-            </div>
+
             <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
                 {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
                     <NavBarItem key={item + index} title={item} />
